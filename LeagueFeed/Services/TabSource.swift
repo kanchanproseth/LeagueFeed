63055a3ec0683ae5a75683547433b52f7bdb4ac8
kanchanproseth <kanchanproseth@users.noreply.github.com>
2017-11-13 14:59:51 +0700

    first commit

---
+++ LeagueFeed/Services/TabSource.swift
@@ -0,0 +1,9 @@
+//
+//  TabSource.swift
+//  LeagueFeed
+//
+//  Created by kanchanproseth on 11/13/17.
+//  Copyright © 2017 kanchanproseth. All rights reserved.
+//
+
+import Foundation
---
+++ LeagueFeed/View/Cell/ChampCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChampCell.swift
+//  LeagueFeed
+//
+//  Created by kanchanproseth on 11/13/17.
+//  Copyright © 2017 kanchanproseth. All rights reserved.
+//
+
+import UIKit
+
+class ChampCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
