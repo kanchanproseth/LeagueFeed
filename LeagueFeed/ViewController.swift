133b0321535bd73be5a6e2b4df2d38de2b0077f5
kanchanproseth <kanchanproseth@users.noreply.github.com>
2017-11-13 13:40:04 +0700

    Initial Commit

---
+++ LeagueFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeagueFeed
+//
+//  Created by kanchanproseth on 11/13/17.
+//  Copyright © 2017 kanchanproseth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
